25170c3ed346f50ec69e2ef041f77a83e0929d03
AndreaBT <andreaturriza@gmail.com>
2024-02-16 10:29:51 -0600

    modifico la url del css

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
             <title>Cuentas BANAVIM</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
             <!-- ESTILOS -->
-            <link href="datosviolencia/css/style.css" rel="stylesheet">
+            <link href="css/style.css" rel="stylesheet">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
